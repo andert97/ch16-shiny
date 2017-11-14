fdc8b392208a47eaa4b0d1a43564e4d103910bd2
Tabitha-Anderson <andert97@uw.edu>
2017-11-14 13:55:46 -0800

    created a function for a plot

--- exercise-1/scripts/BuildScatter.R
+++ exercise-1/scripts/BuildScatter.R
@@ -8,3 +8,10 @@
 #   - Title of the plot (set a default of "Title")
 #   - Label for the x axis (set a default of "X Title")
 #   - Label for the y axis (set a default of "Y Title")
+librarY(plotly)
+BuildScatter <- function(df, x, y) {
+  x.point = df$x
+  y.point = df$y
+  plot_ly(data = ~df, x =  ~x.point, y = ~y.point,  color = "Blue")
+}
+  
